de9326bbfc702da1a6f3042f0e24021d386798bd
e <735647571@qq.com>
2020-10-21 16:54:16 +0800

    day1 学习数据处理

---
+++ day-1/day_1.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
+
+imputer = SimpleImputer(missing_values=np.NAN, strategy="mean", fill_value=None, verbose=0, copy=True)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+
+# print(X)
+
+onehotencoder = OneHotEncoder()
+
+abc = np.array(X[:, 0])
+abc = abc.reshape(-1, 1)
+print(abc)
+
+abc = onehotencoder.fit_transform(abc).toarray()
+
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
+print(abc)
+print()
+print(X)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+# print(X_train)
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
+# print()
+# print(X_train)
+# print()
+# # print(X_test)
